72fecf7ff50acf04a79b7e7ec4803498b1294ac9
Osvaldss <zadeikisosvaldas@gmail.com>
2021-07-24 18:07:35 +0000

    Add js to show loading image while difficulty is not choosen yet

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $(".card-container").append('<h2>Please Choose Difficulty</h2><img src="assets/images/loader.gif" width="250px" height="250px" alt="loading"/>')
+
+});
+
+
+
