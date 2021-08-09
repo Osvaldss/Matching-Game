ba15dacbe238dfb7de17f5ce9636987795f29f4b
Osvaldss <zadeikisosvaldas@gmail.com>
2021-08-09 00:58:01 +0000

    Tidy up script code and split game mode code into seperate functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,51 @@
 $(document).ready(function () {
+    
+});
+ //---------------------- when easy button clicked create easy difficulty game mode with 8 cards and 4 icon pairs from images array
+ $('#easy').click(function() {
 
+    $('.card-container').children('.card').remove();
+    $('.card-container').find('#loader').addClass('hidden');
+    gameScreen();
+    createCards(8);
+    addImagesToCards();
+    shuffleCards();
+    flipCards();
+});
+
+
+//---------------------- when medium button clicked create medium difficulty game mode with 16 cards and 9 icon pairs from images array
+$('#medium').click(function() {
+    $('.card-container').children('.card').remove();
+    $('.card-container').find('#loader').addClass('hidden');
+    gameScreen();
+    createCards(16);
+    addImagesToCards();
+    shuffleCards();
+    flipCards();
+});
+
+
+//---------------------- when hard button clicked create hard difficulty game mode with 24 cards and 12 icon pairs from images array
+$('#hard').click(function() {
+    $('.card-container').children('.card').remove();
+    $('.card-container').find('#loader').addClass('hidden');
+    gameScreen();
+    createCards(24);
+    addImagesToCards();
+    shuffleCards();
+    flipCards();
+});
+
+//----------------------------------------Choose difficulty button
+$('#choose-difficulty').click(function(){
+    $('.card-container').children('.card').remove();
+    $('.card-container').first().children('div').removeClass('hidden');
+    clearInterval(timer);
+    seconds = 0;
+    $('#time-pass').text(seconds);
+    moves = 0;
+    $('#moves').text(moves);
 });
 
 // stackowerflow creating image array
@@ -10,7 +56,6 @@ var createImage = function(src, nameOfClass, title){
     img.alt = title;
     return img;
 };
-
 var images = [];
 images.push(createImage('assets/images/dribble.png', 'icon', 'Icon of Dribble'));
 images.push(createImage('assets/images/facebook.png', 'icon', 'Icon of Facebook'));
@@ -24,47 +69,57 @@ images.push(createImage('assets/images/yahoo.png', 'icon', 'Icon of Yahoo'));
 images.push(createImage('assets/images/youtube.png', 'icon', 'Icon of Youtube'));
 images.push(createImage('assets/images/rss.png', 'icon', 'Icon of RSS Feed'));
 images.push(createImage('assets/images/blogger.png', 'icon', 'Icon of Blogger'));
-console.log(images);
-// stackowerflow creating image array
 
-//---------------------- when easy button clicked create easy difficulty game mode with 8 cards and 4 icon pairs from images array
-$('#easy').click(function() {
-    let cardNumber = 8;
-    let moves = 0;
-    $('#moves').text(moves);
-    $('.card-container').children('.card').remove();
-    $('.card-container').first().children('div').addClass('hidden');
-    //--------------------------- creates cards -------------------------//
+//------------------------------------------- start game screen------------------------------------------//
+function gameScreen() {
+    $('div .start-game-screen').css('display', 'flex').addClass('visible');
+    $('div .start-game-screen').click(function(){
+        $('.start-game-screen').fadeOut('slow', clearInterval(timer), startTimer());
+    });
+}
+
+//---------------------------------------------Create cards----------------------------------------------//
+function createCards(cardNumber) {
     for (let i = 0; i < cardNumber; i++) {
         $('.card-container').append('<div class="card"></div>');
     };
-    $('.card').append('<div class="card-back"></div>').append('<div class="card-front"></div>');
-    //---------------------------------------------add images to each card----------------------------------//
+    $('.card').append('<div class="card-back"></div>').append('<div class="card-front"></div>');  
+};
+
+//---------------------------------------------Add images to each card----------------------------------//
+function addImagesToCards() {
     let allFrontCards = $('.card-front');
     for (let j = 0; j < images.length; j++) {
         allFrontCards.slice( j * 2, j * 2 + 2).append(images[j]);
     };
+}; 
 
-    //--------------------------------------- shuffle cards Fisher–Yates shuffle wikipedia --------------------------------------------//
+//------------------------------------- start timer ---------------------------------------//
+var seconds = 0;
+var timer;
+function startTimer(){
+    timer = setInterval(function () {
+         $('#time-pass').text(seconds);
+            seconds++;
+     }, 1000);
+};
+
+//--------------------------------------- shuffle cards Fisher–Yates shuffle wikipedia --------------------------------------------//
+function shuffleCards() {
     let cards = $('.card');
     for( let i = cards.length - 1; i > 0; i--) {
         let randomIndex = Math.floor(Math.random() * (i + 1));
         cards[randomIndex].style.order = i;
         cards[i].style.order = randomIndex;
     }
+};
 
-    //------------------------------------- time interval ---------------------------------------//
-    var seconds = 1;
-    $('#restart').click(function(){
-        setInterval(function () {
-            $('#time-pass').text(seconds);
-            seconds++;
-        }, 1000);
-    });
-
-    //------------------------------------flip clicked cards-------------------------------------------------//
+//------------------------------------flip clicked cards(count moves) and check for a match-------------------------------------------------//
+function flipCards() {
+    let moves = 0;
+    $('#moves').text(moves);
     $('.card').click(function(){
-        //--- allow to lfip only two cards at a time
+        //--- allow to flip only two cards at a time
         if($('.selected').length >= 2){
             return;
         } else {
@@ -76,13 +131,13 @@ $('#easy').click(function() {
 
         // add selected class
         $(this).addClass('selected flip');
-
+        //----- check if the cards match
         if ($('.selected').length === 2){
             if($('.selected').first().find('img').attr('src') == $('.selected').last().find('img').attr('src')){
                 $('.selected').each(function() {
                     $(this).removeClass('selected').unbind('click');
                 });
-                // remove click eventhandler / play match music
+                // play match music
             } else { 
                 console.log('not matched');
                 setTimeout (function() {
@@ -94,49 +149,6 @@ $('#easy').click(function() {
         };
     };
     });
-});
-
-
-//---------------------- when medium button clicked create medium difficulty game mode with 16 cards and 9 icon pairs from images array
-$('#medium').click(function() {
-    let cardNumber = 16;
-    $('.card-container').children('.card').remove();
-    $('.card-container').first().children('div').addClass('hidden');
-
-    for (let i = 0; i < cardNumber; i++) {
-        $('.card-container').append('<div class="card"></div>');
-    };
-    $('.card').append('<div class="card-back"></div>').append('<div class="card-front"></div>');
-
-    let allFrontCards = $('.card-front');
-    console.log(allFrontCards);
-    for (let j = 0; j < images.length; j++) {
-        allFrontCards.slice( j * 2, j * 2 + 2).append(images[j]);
-    };
-});
-
-//---------------------- when hard button clicked create hard difficulty game mode with 24 cards and 12 icon pairs from images array
-$('#hard').click(function() {
-    let cardNumber = 24;
-    $('.card-container').children('.card').remove();
-    $('.card-container').first().children('div').addClass('hidden');
-
-    for (let i = 0; i < cardNumber; i++) {
-        $('.card-container').append('<div class="card"></div>');
-    };
-    $('.card').append('<div class="card-back"></div>').append('<div class="card-front"></div>');
-
-    let allFrontCards = $('.card-front');
-    console.log(allFrontCards);
-    for (let j = 0; j < images.length; j++) {
-        allFrontCards.slice( j * 2, j * 2 + 2).append(images[j]);
-    };
-});
-
-$('#choose-difficulty').click(function(){
-    $('.card-container').children('.card').remove();
-    $('.card-container').first().children('div').removeClass('hidden');
-
-});
+};
 
 
